e25a10f420fe7d94eec7132b0bde3fcd69f1064a
molexandev01~ <molexandev@gmail.com>
2025-02-12 17:17:58 +0200

    added footer layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-// import './App.scss';
+import './styles/global.scss';
+import './styles/variables.scss';
 
 // import Menu from './components/Menu/Menu';
 import HomePage from './components/HomePage/HomePage';
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './Footer.module.scss';
+import Menu from '../Menu/Menu';
 
 const Footer = () => {
    return (
       <footer className={styles.footer}>
          <div className="container">
-            <p>© 2025 Coffee Shop. All rights reserved.</p>
+            <div className={styles.footer__wrapper}>
+               <div className={styles.footer__inner}>
+                  <Menu />
+               </div>
+               {/* <p>© 2025 Coffee Shop. All rights reserved.</p> */}
+            </div>
          </div>
       </footer>
    );
